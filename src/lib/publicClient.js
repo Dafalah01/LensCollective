e96192be10467055fd9f79493c9da7e07e7b5223
Muhammad Dafa Falah Labib <118614729+Dafalahl@users.noreply.github.com>
2025-05-19 21:34:20 +0700

    Delete src directory

--- src/lib/publicClient.js
+++
@@ -1,8 +0,0 @@
-import { createPublicClient, http } from 'viem';
-import { polygonAmoy } from 'wagmi/chains'; 
-
-
-export const publicClient = createPublicClient({
-  chain: polygonAmoy, 
-  transport: http('https://polygon-amoy.g.alchemy.com/v2/jST9wrgk-7GjCV0s0qIirZp2G5CunVzZ')
-});
